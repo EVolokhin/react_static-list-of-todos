638a859bad3b460764026d83f29bd199e32b3019
Eugene <evolokhin@gmail.com>
2020-10-23 13:53:39 +0300

    Updates

---
+++ precache-manifest.0f81a0f50124186d24b13d84bfc2d04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93e40685dce806d71df983efead8c98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4e83dfcebbb784954ae",
+    "url": "./static/css/main.0031af67.chunk.css"
+  },
+  {
+    "revision": "236e0f5a66d02ca9f43a",
+    "url": "./static/js/2.956c6d12.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.956c6d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e83dfcebbb784954ae",
+    "url": "./static/js/main.df5ee752.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
